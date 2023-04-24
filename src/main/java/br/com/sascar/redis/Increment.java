cf018a5fa575eb55c591c5d4681af662eb62c02a
EricSaDev <eric.almeida.sa@gmail.com>
2023-04-24 16:07:30 -0300

    first commit

---
+++ src/main/java/br/com/sascar/redis/Increment.java
@@ -0,0 +1,14 @@
+package br.com.sascar.redis;
+
+public class Increment {
+    public String key;
+    public int value;
+
+    public Increment(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Increment() {
+    }
+}
